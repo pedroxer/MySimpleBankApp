c6e3600dc54026ea1b5bc63ba08fbdb70b5dd191
Pedroxer <danila.melnikov.53@gmail.com>
2023-05-05 10:40:31 +0300

    Use config load instead hard-coded variables

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ In that project i use
 - sqlc for generating CURD operations
 - require package for testing
 - Gin for web-development
+- Viper for loading the config
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"MelBank/util"
 	"database/sql"
 	"log"
 	"os"
@@ -11,14 +12,15 @@ import (
 
 var testQueries *Queries
 var testDB *sql.DB
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/melbank?sslmode=disable"
-)
+
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil{
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the db:", err)
 	}
