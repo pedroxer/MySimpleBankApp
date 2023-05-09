7bcab9ad7505a2d8cddbd6b887e38d01b8b76c02
Pedroxer <danila.melnikov.53@gmail.com>
2023-05-09 12:27:43 +0300

    Implement transfer api

--- api/server.go
+++ api/server.go
@@ -22,7 +22,7 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PUT("/accounts/update", server.updateAccount)
-	
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
